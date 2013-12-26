59e499b4472a1ac3fcdc3da2e860a4040f851d53
Gregory McIntyre <greg@gregorymcintyre.com>
2013-12-27 09:38:14 +1100

    Various updates.

--- config.rb
+++ config.rb
@@ -1,4 +1,12 @@
+activate :livereload
+
 I18n.enforce_available_locales = false
 
 configure :build do
+  activate :asset_hash
+  activate :minify_css
+  activate :minify_javascript
+  activate :cache_buster
+  activate :relative_assets
+  set :relative_links, true
 end
