1239c708659eecfcdb6de313d0eb6918de9f9cd4
Gregory McIntyre <greg@gregorymcintyre.com>
2017-07-21 16:26:23 +1000

    Even cleaner slides syntax, no ERB required, fix issue with code block layout

--- helpers/slide_helpers.rb
+++
@@ -1,9 +0,0 @@
-module SlideHelpers
-  def markdown_slide(&block)
-    if block_given?
-      concat_content "<section data-markdown><textarea data-template>".html_safe
-      concat_content capture_html(&block)
-      concat_content "</textarea></section>".html_safe
-    end
-  end
-end
